9c1cd3506a6414b70c10e74f89e0ca97e329500c
juanan1996 <54101742+juanan1996@users.noreply.github.com>
2025-02-22 11:17:31 +0100

    Added button to personalize the Grid size
    Hover animation added too

--- javascript.js
+++ javascript.js
@@ -1,34 +1,60 @@
 const styleCss = document.createElement("style");
 document.head.appendChild(styleCss);
 
-styleCss.sheet.insertRule('#container { display: flex ; flex-wrap: wrap; width: 100%;}', 0)
+styleCss.sheet.insertRule('#page { width: 100vw; height: 5vh }', 0);
 
-styleCss.sheet.insertRule('div {flex: 0 1 calc(100%/16); height: calc(90vh/16);outline: 1px solid salmon;}', 0)
+styleCss.sheet.insertRule('.head { height: -webkit-fill-available }', 0);
 
-styleCss.sheet.insertRule('#container div:hover { background-color: #3498db; }', 0);
+cssActive();
 
-styleCss.sheet.insertRule('.head { position: absolute; justify-self: anchor-center;top: 0; display:flex; }', 0);
+function cssActive() {
 
-page = document.createElement("div");
-document.body.append(page)
-page.id = "page"
-page = document.getElementById("page")
+    styleCss.sheet.insertRule('button { display: flex; justify-self: anchor-center; height: -webkit-fill-available; align-items: center; padding: 0 3vw; font-size: 1vw}', 0);
 
-divHead = document.createElement("div");
-page.append(divHead);
-divHead.classList.add("head");
+    styleCss.sheet.insertRule('#container { display: flex ; flex-wrap: wrap; width: 100vw; height: 95vh; max-width: 100vw; max-height: 95vh;}', 0);
 
-button = document.createElement("button");
-button.textContent = "Change"
-divHead.appendChild(button)
-
-container = document.createElement("div");
-document.body.append(container)
-container.id = "container"
-container = document.getElementById("container")
-
-for (i = 0; i < 16 * 16; i++) {
-    content = document.createElement("div");
-    container.append(content);
+    styleCss.sheet.insertRule('#container div:hover {background-color: #3498db; }', 0);
 }
 
+let page = document.createElement("div");
+page.id = "page";
+document.body.append(page);
+
+let divHead = document.createElement("div")
+divHead.classList.add("head");
+page.append(divHead);
+
+let button = document.createElement("button");
+button.textContent = "Change";
+divHead.appendChild(button);
+
+let squares = 0
+
+button.addEventListener("click", () => {
+    resetGrid();
+    let squares = prompt("How many Squares?");
+    if (squares > 100) {
+        squares = 100
+        alert("Can't add more than 100 squares")
+    }
+    createGrid(squares);
+})
+
+let container = document.createElement("div");
+document.body.append(container)
+container.id = "container"
+
+function createGrid(x) {
+    for (i = 0; i < x * x; i++) {
+        content = document.createElement("div");
+        container.append(content);
+    }
+    styleCss.sheet.insertRule('#container div {outline: 1px solid salmon;flex: 0 1 calc(100vw/' + x + '); height: calc(95vh/' + x + ')}', 0);
+
+}
+
+function resetGrid() {
+    container.innerHTML = "";
+    styleCss.sheet.deleteRule('#container div');
+    cssActive();
+}
